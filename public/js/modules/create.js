fd83a13eceb2ed4277c7b9ac84c955fdbdb55a13
SEVERECLICK-dev <papadopouloskaris@gmail.com>
2020-09-30 16:58:47 +0200

    update design Calculatrice

--- public/js/modules/create.js
+++ public/js/modules/create.js
@@ -19,48 +19,16 @@ let myDivRow = document.createElement("div");
 mainDiv.appendChild(myDivRow);
 myDivRow.setAttribute("id", "gridContainer");
 
-for (let i = 1; i <= 16; i++) {
+let tab = [
+    "1", "2", "3", "+",
+    "4", "5", "6", "-",
+    "7", "8", "9", "*",
+    "C", "0", "=", "/"
+];
+
+for (let i = 0; i < tab.length; i++) {
     let gridItem = document.createElement("button");
     gridItem.setAttribute("class", "grid-item");
     myDivRow.appendChild(gridItem);
-    gridItem.innerText = i;
-    if (i == 4) {
-        gridItem.innerText = "+";
-        gridItem.style.backgroundColor = "#0579FB";
-        gridItem.style.color = "white";
-    } else if (i == 5) {
-        gridItem.innerText = "4";
-    } else if (i == 6) {
-        gridItem.innerText = "5";
-    } else if (i == 7) {
-        gridItem.innerText = "6";
-    } else if (i == 9) {
-        gridItem.innerText = "7";
-    } else if (i == 10) {
-        gridItem.innerText = "8";
-    } else if (i == 11) {
-        gridItem.innerText = "9";
-    } else if (i == 8) {
-        gridItem.innerText = "-";
-        gridItem.style.backgroundColor = "#0579FB";
-        gridItem.style.color = "white";
-    } else if (i == 12) {
-        gridItem.innerText = "*";
-        gridItem.style.backgroundColor = "#0579FB";
-        gridItem.style.color = "white";
-    } else if (i == 16) {
-        gridItem.innerText = "/";
-        gridItem.style.backgroundColor = "#0579FB";
-        gridItem.style.color = "white";
-    } else if (i == 13) {
-        gridItem.innerText = "C";
-        gridItem.style.backgroundColor = "#DD4A46";
-        gridItem.style.color = "white";
-    } else if (i == 15) {
-        gridItem.innerText = "=";
-        gridItem.style.backgroundColor = "#40A745";
-        gridItem.style.color = "white";
-    } else if (i == 14) {
-        gridItem.innerText = "0";
-    }
+    gridItem.innerText = tab[i];
 }
