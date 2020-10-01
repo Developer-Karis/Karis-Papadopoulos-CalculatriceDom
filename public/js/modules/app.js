1c70390a63d13ce18a9bbeab0808495f662af9be
SEVERECLICK-dev <papadopouloskaris@gmail.com>
2020-10-01 12:43:55 +0200

    update coding solved input fix and program works

--- public/js/modules/app.js
+++ public/js/modules/app.js
@@ -2,35 +2,85 @@ import { myInput, myDivRow } from './create.js';
 
 myInput.setAttribute("type", "number");
 
+// Ne pas copier coller un lien sur l'input
+myInput.setAttribute("onpaste", "return false");
+// Ne pas avoir le symbole e dans l'input
+myInput.setAttribute("onkeydown", "return event.keyCode !== 69");
+
 let arr = Array.from(myDivRow.children);
 
+let tabOp = ["+", "-", "*", "/"]; 46
 let firstNumber;
-let secondNumber;
 let result;
+let para;
 
 arr.forEach(element => {
     element.addEventListener("click", (e) => {
         if (e.target.innerText == parseInt(e.target.innerText)) {
             myInput.value += e.target.innerText;
-        } else if (e.target.innerText == "+") {
-            firstNumber = myInput.value;
-            console.log(firstNumber);
-            myInput.value = myInput.value.slice(myInput.value.length);
-            secondNumber = myInput.value;
-            result = parseInt(firstNumber) + parseInt(secondNumber);
-        } else if (e.target.innerText == "-") {
-            myInput.value = myInput.value.slice(myInput.value.length);
-            result = parseInt(firstNumber) - parseInt(secondNumber);
-        } else if (e.target.innerText == "*") {
-            myInput.value = myInput.value.slice(myInput.value.length);
-            result = parseInt(firstNumber) * parseInt(secondNumber);
-        } else if (e.target.innerText == "/") {
-            myInput.value = myInput.value.slice(myInput.value.length);
-            result = parseInt(firstNumber) / parseInt(secondNumber);
         } else if (e.target.innerText == "=") {
-            myInput.value = result;
+            if (para.includes("+")) {
+                para = myInput.value + "=";
+                result = parseInt(firstNumber) + parseInt(myInput.value);
+                myInput.value = result;
+            } else if (para.includes("-")) {
+                para = myInput.value + "=";
+                result = parseInt(firstNumber) - parseInt(myInput.value);
+                myInput.value = result;
+            } else if (para.includes("*")) {
+                para = myInput.value + "=";
+                result = parseInt(firstNumber) * parseInt(myInput.value);
+                myInput.value = result;
+            } else if (para.includes("/")) {
+                para = myInput.value + "=";
+                result = parseInt(firstNumber) / parseInt(myInput.value);
+                myInput.value = result;
+            }
         } else if (e.target.innerText == "C") {
-            myInput.value = myInput.value.slice(0, myInput.value.length - 1);
+            myInput.value = "";
+        } else {
+            tabOp.forEach(element => {
+                if (e.target.innerText.includes(element)) {
+                    firstNumber = myInput.value;
+                    para = myInput.value + element;
+                    console.log(para);
+                    myInput.value = "";
+                }
+            });
         }
     })
+});
+
+// Gérer les touches du clavier
+
+$(document).keypress(function (e) {
+    if (e.key >= 0 && e.key <= 9) {
+        myInput.value += e.key;
+    } else if (e.key == "=" || e.key == "Enter") {
+        if (para.includes("+")) {
+            para = myInput.value + "=";
+            result = parseInt(firstNumber) + parseInt(myInput.value);
+            myInput.value = result;
+        } else if (para.includes("-")) {
+            para = myInput.value + "=";
+            result = parseInt(firstNumber) - parseInt(myInput.value);
+            myInput.value = result;
+        } else if (para.includes("*")) {
+            para = myInput.value + "=";
+            result = parseInt(firstNumber) * parseInt(myInput.value);
+            myInput.value = result;
+        } else if (para.includes("/")) {
+            para = myInput.value + "=";
+            result = parseInt(firstNumber) / parseInt(myInput.value);
+            myInput.value = result;
+        }
+    } else {
+        tabOp.forEach(element => {
+            if (e.key.includes(element)) {
+                firstNumber = myInput.value;
+                para = myInput.value + element;
+                myInput.value = "";
+            }
+        });
+    }
 });
